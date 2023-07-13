0462520c399f1515edd3e675221dc4af45ca3da7
VASANTHI.AKAVEETI <VASANTHI.AKAVEETI@DESKTOP-IRBB8HI>
2023-07-13 15:57:38 +0530

    sample commit

---
+++ src/test/java/ish/admintest/ExamTest.java
@@ -0,0 +1,21 @@
+package ish.admintest;
+
+import org.testng.annotations.Test;
+
+public class ExamTest{
+	
+	@Test()
+	
+	public void Sample_test_demo()
+	{
+		System.out.println("demo - new class");
+		
+		System.out.println("hi");
+		
+		System.out.println("demo - new class2");
+		
+		System.out.println("hello");
+		
+	}
+
+}
---
+++ src/test/java/ish/admintest/Practice_test.java
@@ -0,0 +1,22 @@
+package ish.admintest;
+
+import org.testng.annotations.Test;
+
+public class Practice_test {
+	
+	@Test()
+	
+	public void practice_test()
+	{
+		System.out.println("sample");
+		
+		System.out.println("sample2");
+		
+		System.out.println("demo - new class");
+		
+		System.out.println("hello");
+		
+		
+	}
+
+}
